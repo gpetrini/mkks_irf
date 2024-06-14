f2719140711a2c1ec4921c007afbea50e12cdc82
gpetrini <gpetrinidasilveira@gmail.com>
2024-06-14 12:22:54 +0200

    Reduce the number of relative pointers

--- jeec_firms_financial.h
+++ jeec_firms_financial.h
@@ -28,9 +28,9 @@ WRITE("DemandLoansForBankRiskEvaluation",
       v[4]); // write the parameter demand for loans that is used in bank's risk
              // evaluation. This value is updated to disconsider investment in
              // case the loan is not granted.
-if (VS(GRANDPARENT, "flagRationing")) // is the credit rationing flag activated?
-{                                     // yes, find banks
-  v[5] = VS(PARENT, "epsilon");       // number of banks to search
+if (VS(ROOT, "flagRationing")) // is the credit rationing flag activated?
+{                              // yes, find banks
+  v[5] = VS(ECO, "epsilon");   // number of banks to search
   if (v[5] > 1) {
     v[6] = v[5] - 1;   // consider only from the second bank onwards
     while (v[6] > 0) { // since firms try to keep relation with the same bank
@@ -46,7 +46,7 @@ RESULT(v[4])
 
 EQUATION("ProbabilityOfDefault")
 /*Calculate the probability of default of a loan project.*/
-if (!VS(root, "flagRationing") || V("DemandLoans") == 0)
+if (!VS(ROOT, "flagRationing") || V("DemandLoans") == 0)
   END_EQUATION(0);
 
 v[2] = V("DemandLoansForBankRiskEvaluation"); // total loan demand
@@ -55,10 +55,10 @@ if (v[2] == 0)
   END_EQUATION(0);
 
 v[0] = V("ExpectedOperatingCashFlow");
-v[1] = VS(GRANDPARENT, "varPhi1");
-v[7] = VS(GRANDPARENT, "varPhi2");
+v[1] = VS(ECO, "varPhi1");
+v[7] = VS(ECO, "varPhi2");
 v[8] = VL("Leverage", 1);
-v[3] = VS(GRANDPARENT, "lambda"); // duration of loan
+v[3] = VS(SEC, "lambda");         // duration of loan
 v[4] = V("InterestAgreedBank");   // get interest rate
 v[5] = v[2] / v[3] + v[4] * v[2]; // debt servicing of this loan project.
 v[6] =
@@ -67,7 +67,7 @@ RESULT(v[6])
 
 EQUATION("ExpectedReturnOfLoanProject")
 v[0] = V("InterestAgreedBank");
-v[1] = VS(GRANDPARENT, "lambda");
+v[1] = VS(SEC, "lambda");
 v[2] = V("ProbabilityOfDefault");
 if (v[2] > 1e-04)
   v[3] = 1 + v[0] * (v[1] + 1) / 2 -
@@ -177,7 +177,7 @@ EQUATION("WageBill")
 Firm's wage bill.
 Level: firm.
 */
-v[0] = VS(GRANDPARENT, "NominalWage");
+v[0] = VS(ECO, "NominalWage");
 v[1] = V("WorkersFirm");
 RESULT(v[0] * v[1])
 
@@ -187,7 +187,7 @@ The expected wage bill is used by firms to demand loans to banks.
 It may differ from the actual wage bill if a firm is credit rationed and/or if
 it's labour rationed. Level: firm.
 */
-v[0] = VS(GRANDPARENT, "NominalWage");
+v[0] = VS(ECO, "NominalWage");
 v[1] = V("DesiredDemandLabour");
 v[2] = v[0] * v[1];
 RESULT(v[2])
@@ -198,7 +198,7 @@ Function to draw a bank randomly. Used in the matching of firms and banks.
 Level: firm.
 */
 V("updateProbDrawBank");
-cur = RNDDRAW("Banks", "probDrawBank");
+cur = RNDDRAWS(ECO, "Banks", "probDrawBank");
 RESULT(VS(cur, "idBank"))
 
 EQUATION("InterestAgreedBank")
@@ -206,13 +206,11 @@ EQUATION("InterestAgreedBank")
 Store interest rate agreed with bank in the firm object.
 Level: firm.
 */
-cur1 = SHOOK == NULL ? SEARCH_CND("idBank", V("BankSupplierID"))
-                     : SHOOK; // gather bank data
-WRITES(cur1, "DebtServicingRatio",
+WRITES(BNK, "DebtServicingRatio",
        VL("DebtServicingToOperatingCashFlowRatio",
           1)); // pass information of lagged debt servicing to operating cash
                // flow ratio (interest determination)
-v[0] = VS(cur1, "InterestRateBank"); // get interest rate
+v[0] = VS(BNK, "InterestRateBank"); // get interest rate
 RESULT(v[0])
 
 EQUATION("DefineCreditLimit")
@@ -223,8 +221,8 @@ Level: firm (for ease of data access).
 cur1 = SHOOK == NULL ? SEARCH_CND("idBank", V("BankSupplierID"))
                      : SHOOK; // gather bank data
 v[0] = V("InterestAgreedBank");
-v[1] = VLS(cur1, "AverageBankInterestRate", 1);
-v[2] = VS(GRANDPARENT, "lambda");
+v[1] = VLS(BNK, "AverageBankInterestRate", 1);
+v[2] = VS(SEC, "lambda");
 v[3] = 50;    // max iterations
 v[4] = 0;     // initialise iteration count
 v[5] = 0.05;  // initial guess probability of default
@@ -243,8 +241,8 @@ while (v[4] < v[3] && v[6] > v[7]) {
 if (v[5] > V("ProbabilityOfDefault"))
   PLOG("\nOpa! Deu ruim!");
 v[10] = V("ExpectedOperatingCashFlow");
-v[11] = VS(GRANDPARENT, "varPhi1");
-v[12] = VS(GRANDPARENT, "varPhi2");
+v[11] = VS(ECO, "varPhi1");
+v[12] = VS(ECO, "varPhi2");
 v[13] = VL("Leverage", 1);
 v[14] = v[11] * v[10] / (log((1 - v[5]) / v[5]) + v[12] * v[13]);
 v[15] = v[14] / (1 / v[2] + v[0]);
@@ -257,7 +255,7 @@ Credit supply of banks.
 Equal to demand if there is no credit rationing.
 Level: firm.
 */
-if (VS(root, "flagRationing")) {
+if (VS(ROOT, "flagRationing")) {
   v[0] = V("ExpectedReturnOfLoanProject");
   if (v[0] >= 1)
     v[1] = V("DemandLoans");
@@ -451,7 +449,7 @@ Level: firm.
 */
 v[0] = V("etaf1");
 v[1] = V("DebtServicingToOperatingCashFlowRatio");
-v[2] = VS(PARENT, "varepsilon");
+v[2] = VS(BNK, "varepsilon");
 v[3] = 2 * v[0] / (1 + exp(v[1] * v[2]));
 RESULT(v[3])
 
@@ -488,7 +486,7 @@ Level: firm
 */
 // force execution at the bank level amortisation computation together with this
 // equation.
-CYCLES(GRANDPARENT, cur, "Banks") { VS(cur, "AmortisationDue"); }
+SUMS(BNK, "AmortisationDue");
 RESULT(SUM("AmortisationSchedule"))
 
 EQUATION("InterestPayment")
@@ -498,13 +496,10 @@ Level: firm.
 */
 v[0] = 0; // accumulator
 CYCLE(cur, "OutstandingLoan") {
-  cur1 = SHOOK == NULL ? SEARCH_CND("idBank", V("BankSupplierID"))
-                       : SHOOK; // search the supplier bank
-
-  if (VS(root, "flagFloatingInterestRate"))
+  if (VS(ROOT, "flagFloatingInterestRate"))
     WRITE("LoanInterestRate",
-          VS(cur1, "InterestRateBank")); // if the interest rate is floating,
-                                         // get bank's current rate
+          VS(BNK, "InterestRateBank")); // if the interest rate is floating,
+                                        // get bank's current rate
 
   v[1] = VS(cur, "LoanInterestRate");
   v[2] = VLS(cur, "LoanOutstanding", 1);
@@ -544,19 +539,17 @@ v[0] = V("DefaultFlag");
 if (!v[0]) // is the firm leaving the market? no. In case of default, the debt
            // servicing is updated elsewhere.
   CYCLE(cur1, "OutstandingLoan") {
-    cur = SHOOK == NULL ? SEARCH_CND("idBank", V("BankSupplierID"))
-                        : SHOOK; // search the supplier bank
 
-    v[1] = VS(cur, "InterestIncome");    // get current interest income (bank)
+    v[1] = VS(BNK, "InterestIncome");    // get current interest income (bank)
     v[2] = VS(cur1, "InterestSchedule"); // get interest schedule (firm)
-    WRITES(cur, "InterestIncome",
+    WRITES(BNK, "InterestIncome",
            v[1] + v[2]); // update interest income of bank with firm payment
 
     v[3] = VS(
-        cur,
+        BNK,
         "AmortisationFlow"); // get amount of amortisation received by the bank
     v[4] = VS(cur1, "AmortisationSchedule"); // get amortisation schedule
-    WRITES(cur, "AmortisationFlow",
+    WRITES(BNK, "AmortisationFlow",
            v[3] + v[4]); // update amortisation schedule
 
     v[5] = VLS(cur1, "LoanOutstanding", 1);       // get loan outstanding
@@ -591,29 +584,26 @@ else {
   v[17] = V("CreditSupply") - v[16]; // accumulator of firms' default
   v[9] -= v[16]; // exclude payment of current period's debt out of available
                  // resources
-  cur = SEARCH_CND("idBank", V("BankID"));
-  WRITES(cur, "AmortisationFlow",
-         VS(cur, "AmortisationFlow") +
+  WRITES(BNK, "AmortisationFlow",
+         VS(BNK, "AmortisationFlow") +
              v[16]); // update amortisation flow of bank that granted credit
                      // this period
-  WRITES(cur, "NonPerformingLoans",
-         VS(cur, "NonPerformingLoans") + v[17]); // update default of the bank
+  WRITES(BNK, "NonPerformingLoans",
+         VS(BNK, "NonPerformingLoans") + v[17]); // update default of the bank
   WRITE("FirmDefault", (1 - v[15]) * v[10]);
 
   CYCLE(cur1, "OutstandingLoan") {
     if (v[9] > 0) {
-      cur = SHOOK == NULL ? SEARCH_CND("idBank", V("BankSupplierID"))
-                          : SHOOK;         // search the supplier bank
-      v[1] = VS(cur, "InterestIncome");    // get current interest income (bank)
+      v[1] = VS(BNK, "InterestIncome");    // get current interest income (bank)
       v[2] = VS(cur1, "InterestSchedule"); // get interest schedule (firm)
       v[3] = v[2] * v[14];                 // interest due that can be paid
       v[9] -= v[3];                        // reduce deposits accordingly
       v[16] += v[3];                       // accumulate total amount paid
       v[17] += v[2] - v[3]; // accumulate default (includes interest)
-      WRITES(cur, "InterestIncome",
+      WRITES(BNK, "InterestIncome",
              v[1] + v[3]); // update interest income of bank with firm payment
 
-      v[4] = VS(cur, "AmortisationFlow"); // get amount of amortisation received
+      v[4] = VS(BNK, "AmortisationFlow"); // get amount of amortisation received
                                           // by the bank
       v[5] = VS(cur1, "LoanOutstanding"); // get loan outstanding
       v[6] =
@@ -624,19 +614,19 @@ else {
           v[6]; // non-performing loans of banks (includes write-off of loans)
       v[16] += v[6]; // accumulate total amount paid
       v[17] += v[8]; // accumulate total default (include interest)
-      WRITES(cur, "AmortisationFlow",
+      WRITES(BNK, "AmortisationFlow",
              v[4] + v[6]); // update amortisation flow of bank
 
-      v[7] = VS(cur, "NonPerformingLoans");
+      v[7] = VS(BNK, "NonPerformingLoans");
       v[18] =
           v[8] + v[2] - v[3]; // default (include interest not paid to banks)
 
       if (v[8] > 0) {
-        WRITES(cur, "NonPerformingLoans",
+        WRITES(BNK, "NonPerformingLoans",
                v[7] + v[8]); // update total non-performing loans of bank (does
                              // not include interest)
-        WRITES(cur, "DefaultLoans",
-               VS(cur, "DefaultLoans") +
+        WRITES(BNK, "DefaultLoans",
+               VS(BNK, "DefaultLoans") +
                    v[18]); // update total default on loans
       }
 
@@ -649,14 +639,12 @@ else {
   if (v[9] > 0) // if there is some remaining cash balance, distribute between
                 // households
   {
-    v[18] = COUNTS(GRANDPARENT, "Households");
-    CYCLES(GRANDPARENT, cur2, "Households") {
-      WRITES(cur2, "CapitalContribution",
-             VS(cur2, "CapitalContribution") - v[9] / v[18]);
-      WRITE("FirmDeposits_PostDividendsUpdate",
-            V("FirmDeposits_PostDividendsUpdate") -
-                v[9] / v[18]); // deduce deposits of this transfer to households
-    }
+    v[18] = COUNTS(ECO, "Households");
+    WRITES(HHS, "CapitalContribution",
+           VS(HHS, "CapitalContribution") - v[9] / v[18]);
+    WRITE("FirmDeposits_PostDividendsUpdate",
+          V("FirmDeposits_PostDividendsUpdate") -
+              v[9] / v[18]); // deduce deposits of this transfer to households
   }
   RECALC("GrossProfits"); // consider the changed interest payments into the
                           // gross and net profits
@@ -678,7 +666,7 @@ V("DebtServicing");
 v[1] = 0;
 if (V("CreditSupply") > 0 && !V("DefaultFlag")) {
   cur = ADDOBJ("OutstandingLoan");
-  v[0] = VS(GRANDPARENT, "lambda");
+  v[0] = VS(ECO, "lambda");
   WRITES(cur, "LoanOutstanding", V("CreditSupply"));
   WRITES(cur, "BankSupplierID", V("BankID"));
   WRITES(cur, "AmortisationSchedule", V("CreditSupply") / v[0]);
