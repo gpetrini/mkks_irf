d7ffed0b89381e953cdbf6372dbbf5e5c0d74bfb
gpetrini <gpetrinidasilveira@gmail.com>
2024-08-14 14:23:03 +0200

    Removes exps from the MC creator

--- data-irf/lsd_MC_runner.sh
+++ data-irf/lsd_MC_runner.sh
@@ -5,8 +5,7 @@
 # File modifiled to generage lsd configurations without reliong on the LSD GUI  #
 #################################################################################
 
-Help()
-{
+Help() {
   # Display help
   printf "These are the program's options: \n"
   printf "\n -h: Help and quit."
@@ -18,9 +17,6 @@ Help()
   printf "\n -l: Path to the lsdNW binary"
   printf "\n -K: Alter the number of experiments. 1 means only the default (no-shock) configuration."
 }
-
-
-
 
 ############################################################
 # Main program                                             #
@@ -43,65 +39,65 @@ EXP=7 ## Baseline + 6 experiments
 while getopts "hd:b:u:M:p:l:K" option; do # read options included in the command line, create a list and loop through the list
   case $option in
 
-    h) # display help
-      Help
-      exit 0;; # quits the program, no additional option is checked.
+  h) # display help
+    Help
+    exit 0
+    ;; # quits the program, no additional option is checked.
 
-    d) # alter directory in which the script is executed
-      FOLDER=$OPTARG;;
+  d) # alter directory in which the script is executed
+    FOLDER=$OPTARG ;;
 
-    b) # base LSD file name (without .lsd extension)
-       # FIXME Check if lsd extension is passed
-      LSDCONF=$OPTARG;;
+  b) # base LSD file name (without .lsd extension)
+    # FIXME Check if lsd extension is passed
+    LSDCONF=$OPTARG ;;
 
-    u) # alter number of processing units
-      UNIT=$OPTARG;;
+  u) # alter number of processing units
+    UNIT=$OPTARG ;;
 
-    M) # Number of monte carlo copies of LSD iles
-      MONTECARLO=$OPTARG;;
+  M) # Number of monte carlo copies of LSD iles
+    MONTECARLO=$OPTARG ;;
 
-    p) # update number of periods in simulation
-      PERIODS=$OPTARG;;
+  p) # update number of periods in simulation
+    PERIODS=$OPTARG ;;
 
-    l) # update NW executable path
-      LSD_EXEC=$OPTARG;;
+  l) # update NW executable path
+    LSD_EXEC=$OPTARG ;;
 
-    K) # change the number of experiments
-      EXP=$OPTARG;;
+  K) # change the number of experiments
+    EXP=$OPTARG ;;
 
-    \?) # invalid option
-       echo "Error: invalid option. Run -h option for help."
-       exit 2;; # quits the program, no additional option is checked.
+  \?) # invalid option
+    echo "Error: invalid option. Run -h option for help."
+    exit 2
+    ;; # quits the program, no additional option is checked.
 
   esac # close cases
 done
 
 # declare folder (if specified by user, otherwise current directory is used)
-if [ -d "$FOLDER" ] ; then
-    printf 'Working directory is: %s \n' "$FOLDER"
+if [ -d "$FOLDER" ]; then
+  printf 'Working directory is: %s \n' "$FOLDER"
 else
-    printf "Provided directory does not exist. Please create directory and rerun."
-    exit 3
+  printf "Provided directory does not exist. Please create directory and rerun."
+  exit 3
 fi
 
 # check if base file exists and declare base file
-if [ -f "$FOLDER/$LSDCONF.lsd" ] ; then
+if [ -f "$FOLDER/$LSDCONF.lsd" ]; then
   printf 'Base file is %s and is located under %s, continuing.\n' "$LSDCONF.lsd" "$FOLDER"
   BASEFULL="$FOLDER/$LSDCONF" # update complete address of base file
 else
-  printf 'Selected file %s does not exist or it is not located under %s. Please create file and rerun.\n'  "$LSDCONF.lsd"  "$FOLDER"
+  printf 'Selected file %s does not exist or it is not located under %s. Please create file and rerun.\n' "$LSDCONF.lsd" "$FOLDER"
   exit 3
 fi
 
 # alter number of periods
 
-if [ "$PERIODS" ] ; then
+if [ "$PERIODS" ]; then
   sed -i -r "s/^(MAX_STEP) (.*)/MAX_STEP $PERIODS/" "$BASEFULL.lsd"
   printf "\nNumber of periods in %s updated:" "$LSDCONF.lsd"
   sed -n '/MAX_STEP/p' "$BASEFULL.lsd"
 fi
-
-
 
 BASEDIR="$FOLDER/$LSDCONF"
 if [ -d "$BASEDIR" ]; then
@@ -122,27 +118,23 @@ else
   mkdir -p "$BASEDIR"
 fi
 
-
-
 for j in $(seq 1 "$EXP"); do
-  SCENARIO=$((j-1))
+  SCENARIO=$((j - 1))
   DEST_FILE="$BASEDIR/${LSDCONF}${j}_1.lsd"
   cp "$FOLDER/$LSDCONF.lsd" "$DEST_FILE"
   sed -i -r "s/^(Param: Scenario) (.*)([\t ])[0-9\.]+\$/\1 \2\3$SCENARIO/" "$DEST_FILE"
 done
-
 
 for j in $(seq 1 "$EXP"); do
   for i in $(seq 1 "$MONTECARLO"); do
     TEMPLATE="$BASEDIR/${LSDCONF}${j}_1.lsd"
     DEST_FILE="$BASEDIR/${LSDCONF}${j}_$i.lsd"
     if [ "$TEMPLATE" != "$DEST_FILE" ]; then
-    cp "$TEMPLATE" "$DEST_FILE"
+      cp "$TEMPLATE" "$DEST_FILE"
     fi
     sed -i -r "s/^(SEED) (.*)/SEED $i/" "$DEST_FILE"
   done
 done
-
 
 EXPS=$(find "$BASEDIR" -type f -name "*.lsd" | wc -l)
 printf "\nFolder %s populated with %d configuration files\n" "$BASEDIR" "$EXPS"
@@ -152,27 +144,23 @@ TOTAL_RUNS=$MONTECARLO
 RUNS_PER_CORE=$((TOTAL_RUNS / UNIT))
 REMAINDER=$((TOTAL_RUNS % UNIT))
 
-for j in $(seq 1 "$EXP"); do
+SCRIPT="$BASEDIR/${LSDCONF}_1_$MONTECARLO.sh"
 
-  SCRIPT="$BASEDIR/${LSDCONF}${j}_1_$MONTECARLO.sh"
+start=1
 
-  start=1
-
-  for (( i=1; i<=UNIT; i++ )); do
-    end=$((start + RUNS_PER_CORE - 1))
-    if [[ $i -le $REMAINDER ]]; then
+for ((i = 1; i <= UNIT; i++)); do
+  end=$((start + RUNS_PER_CORE - 1))
+  if [[ $i -le $REMAINDER ]]; then
     end=$((end + 1))
-    fi
-    CMD="nice ../$LSD_EXEC -c 1 -f ./$LSDCONF${j} -s $start -e $end -p -l ./${LSDCONF}${j}_$i.log &"
-    echo "$CMD" >> "$SCRIPT"
+  fi
+  CMD="nice ../$LSD_EXEC -c 1 -f ./$LSDCONF${j} -s $start -e $end -p -l ./${LSDCONF}${j}_$i.log &"
+  echo "$CMD" >>"$SCRIPT"
 
-    start=$((end + 1))
-  done
-  echo "echo \"$UNIT log files being generated.\"" >> "$SCRIPT"
-
-  # Make the generated script executable
-  chmod +x "$SCRIPT"
-
-  printf "Script %s generated and made executable.\n" "$SCRIPT"
-
+  start=$((end + 1))
 done
+echo "echo \"$UNIT log files being generated.\"" >>"$SCRIPT"
+
+# Make the generated script executable
+chmod +x "$SCRIPT"
+
+printf "Script %s generated and made executable.\n" "$SCRIPT"
